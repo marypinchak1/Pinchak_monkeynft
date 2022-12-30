3dcc7eb69b8d9b42687208a5a9927de341cf4d4a
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-30 02:36:48 +0200

    add-header

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,22 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
+$(".header__burger").on("click", function () {
+  $(".header__burger").toggleClass("active");
+  $(".header__burger_menu").toggleClass("active");
+  gsap.fromTo(
+    ".header__burger_menu",
+    {
+      scale: 1.2,
+    },
+    {
+      duration: 1,
+      scale: 1,
+    }
+  );
+  gsap.from(".header__burger_menu li", {
+    duration: 1,
+    x: -1000,
   });
 });
-
+$(".header__burger_menu a").on("click", function () {
+  $(".header__burger_menu").removeClass("active");
+  $(".header__burger").removeClass("active");
+});
